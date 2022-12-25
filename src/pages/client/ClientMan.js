49166159dc74c49942de1a1abfb9d7279ae9571c
adiljin <adiljins@gmail.com>
2022-12-25 18:15:53 -0500

    Added:  - ClientMan.js

--- src/pages/client/ClientMan.js
+++ src/pages/client/ClientMan.js
@@ -10,7 +10,7 @@ export default function ClientMan() {
                     <div class="d-grid gap-2 col-6 mx-auto">
                         <Link class="btn btn-dark" type="button" to={`/pages/clientman/addclient`}>Register shipping company</Link>
                         <button class="btn btn-dark" type="button">Edit shipping company</button>
-                        <button class="btn btn-dark" type="button">View/Delete shipping company</button>
+                        <button class="btn btn-dark" type="button">View shipping company</button>
                         <Link class="btn btn-dark my-4" type="button" to={`/pages/main`}>Return</Link>
                     </div>
                 }
