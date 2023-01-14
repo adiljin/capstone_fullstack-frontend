6aee7415577317259626f7a37327f29c40153cef
adiljin <adiljins@gmail.com>
2023-01-14 01:37:09 -0500

    Styling login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,6 +2,17 @@ import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useLocalState } from '../../util/useLocalStorage';
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBInput,
+    MDBIcon
+}
+    from 'mdb-react-ui-kit';
 
 
 export default function Login() {
@@ -46,30 +57,61 @@ export default function Login() {
     }
 
     return (
-        <>
-            <div>
-                <label htmlFor='username'>Username</label>
-                <input
-                    type={"username"}
-                    id="username"
-                    name="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)} />
-            </div>
-            <div>
-                <label htmlFor='password'>Password</label>
-                <input
-                    type={"password"}
-                    id="password"
-                    name="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <div>
-                <button id='submit' type='button' onClick={() => sendLoginRequest()}>
-                    Submit
-                </button>
-            </div>
-        </>
+
+        <MDBContainer fluid>
+
+            <MDBRow className='d-flex justify-content-center align-items-center h-100'>
+                <MDBCol col='12'>
+
+                    <MDBCard className='bg-dark text-white my-5 mx-auto' style={{ borderRadius: '1rem', maxWidth: '400px' }}>
+                        <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
+
+                            <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                            <p className="text-white-50 mb-5">Please enter your username and password!</p>
+
+                            <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Username' id="username" name="username" value={username} type={"username"} onChange={(e) => setUsername(e.target.value)} size="lg" />
+                            <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Password' id="password" name="password" value={password} type={"password"} onChange={(e) => setPassword(e.target.value)} size="lg" />
+
+                            <button class="btn btn-dark btn-lg" id='submit' type='button' onClick={() => sendLoginRequest()}>
+                                Login
+                            </button>
+
+                        </MDBCardBody>
+                    </MDBCard>
+
+                </MDBCol>
+            </MDBRow>
+
+        </MDBContainer>
+
+        // <div classNameName='container my-12'>
+        //     <div classNameName='row'>
+        //         <div classNameName='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
+        //             <div classNameName='mb-2 mx-1'>
+        //                 <label htmlFor='username'>Username</label>
+        //                 <input
+        //                     type={"username"}
+        //                     id="username"
+        //                     name="username"
+        //                     value={username}
+        //                     onChange={(e) => setUsername(e.target.value)} />
+        //             </div>
+        //             <div classNameName='mb-2 mx-1'>
+        //                 <label htmlFor='password'>Password</label>
+        //                 <input
+        //                     type={"password"}
+        //                     id="password"
+        //                     name="password"
+        //                     value={password}
+        //                     onChange={(e) => setPassword(e.target.value)} />
+        //             </div>
+        //             <div>
+        //                 <button id='submit' type='button' onClick={() => sendLoginRequest()}>
+        //                     Submit
+        //                 </button>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
