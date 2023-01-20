4633516ec6ebc6e389655cd66c3029940a886933
adiljin <adiljins@gmail.com>
2023-01-20 15:07:12 -0500

    Added fetchService.js for token validation
    Changed:
    PrivateRoute.js, additional functionality for token validation

--- src/pages/privateRoute/PrivateRoute.js
+++ src/pages/privateRoute/PrivateRoute.js
@@ -1,10 +1,22 @@
+import { get } from 'jquery';
 import React, { useState } from 'react'
 import { Navigate } from 'react-router-dom';
+import ajax from '../../services/fetchService';
 import { useLocalState } from '../../util/useLocalStorage';
 
 export const PrivateRoute = ({ children }) => {
     const [jwt, setJwt] = useLocalState("", "jwt");
-    return (
-        jwt ? children : <Navigate to={"/login"} />
-    )
+    const [isLoading, setIsLoading] = useState(true);
+    const [isValid, setIsValid] = useState(null);
+
+    if(jwt){
+        ajax(`/api/auth/validate?token=${jwt}`, "get", jwt).then(isValid=>{
+            setIsValid(isValid);
+            setIsLoading(false);
+        })
+    }else{
+        return <Navigate to={"/login"} />
+    }
+    // return jwt ? children : <Navigate to={"/login"} />
+    return isLoading ? <div>Loading</div> : isValid === true ? children : <Navigate to={"/login"} />
 }
