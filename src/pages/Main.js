3720be9bb1a97f07d401cf9bad85969fb9e16205
adiljin <adiljins@gmail.com>
2023-01-25 15:48:19 -0500

    Added log out button functionality

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -13,7 +13,10 @@ export default function Main() {
                         <Link class="btn btn-dark" type="button" to={`/pages/RoutMan`}>Routes Management</Link>
                         <Link class="btn btn-dark" type="button" to={`/pages/FreMan`}>Freights Management</Link>
                         <button class="btn btn-dark" type="button">Manage Users</button>
-                        <button class="btn btn-dark my-4" type="button">Change User</button>
+                        <button class="btn btn-dark my-4" type="button" onClick={() => {
+                            localStorage.clear();
+                            window.location.href = "/login";
+                        }}>Logout</button>
                     </div>
                 }
             </div>
